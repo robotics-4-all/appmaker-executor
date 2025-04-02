e97675ea0919d5cc12fbc7977de11f54791a6437
Konstantinos Panayiotou <klpanagi@gmail.com>
2025-04-02 10:29:29 +0300

    Configure redis parameters via envs

--- appmakerexecutor/src/appmakerexecutor/appmaker_executor.py
+++ appmakerexecutor/src/appmakerexecutor/appmaker_executor.py
@@ -23,6 +23,7 @@ from commlib.transports.redis import ConnectionParameters as RedisConnectionPara
 from appmaker_node import AppMakerNode # type: ignore # pylint: disable=import-error
 from appmaker_node_executor import NodeExecutor # type: ignore # pylint: disable=import-error
 from appmaker_storage import StorageHandler # type: ignore # pylint: disable=import-error
+from config import REDIS_HOST, REDIS_PORT, REDIS_DB, REDIS_PASSWORD, REDIS_USERNAME
 
 class AppMakerExecutor(CommlibNode):
     """
@@ -83,7 +84,13 @@ class AppMakerExecutor(CommlibNode):
 
         # Local interfaces
         self.local_commlib_node = CommlibNode(node_name='sslocsys.app_executor_node_local',
-            connection_params=RedisConnectionParameters(),
+            connection_params=RedisConnectionParameters(
+                host=REDIS_HOST,
+                port=REDIS_PORT,
+                username=REDIS_USERNAME,
+                password=REDIS_PASSWORD,
+                db=REDIS_DB
+            ),
             heartbeats=False,
             debug=True)
 
--- appmakerexecutor/src/appmakerexecutor/config.py
+++ appmakerexecutor/src/appmakerexecutor/config.py
@@ -2,3 +2,9 @@ import os
 
 ZERO_LOGS = int(os.getenv('APPMAKER_ZERO_LOGS', 0))
 LOG_LEVEL = os.getenv("APPMAKER_LOG_LEVEL", "INFO")
+
+REDIS_HOST=os.getenv("REDIS_HOST", "localhost")
+REDIS_PORT=os.getenv('REDIS_PORT', '6379')
+REDIS_USERNAME=os.getenv('REDIS_USERNAME', '')
+REDIS_PASSWORD=os.getenv('REDIS_PASSWORD', '')
+REDIS_DB=os.getenv('REDIS_DB', 0)
